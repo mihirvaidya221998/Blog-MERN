1c34d5f2b0c4bff97c2a95d9da7b0681440cb100
Mihir Vaidya <mihirsv22@gmail.com>
2024-06-10 16:11:36 -0400

    Implemented Navbar

--- blog_client/src/App.jsx
+++ blog_client/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <div className='text-2xl'>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='about' element={<About/>}/>
