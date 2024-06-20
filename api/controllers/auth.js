5da8db7af5a594ddd50d734e49256eccbdb8e9c9
Mihir Vaidya <mihirsv22@gmail.com>
2024-06-20 15:49:00 -0400

    Worked on the backend of SignIn Page.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from '../models/user.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 //SignUp Controller
 export const signup = async(req, res, next) =>{
@@ -21,3 +22,32 @@ export const signup = async(req, res, next) =>{
     }
     
 }
+
+//Signin Controller
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    if(!email || !password || email==='' || password === ''){
+        next(errorHandler(400, 'All Fields are Required!'));
+    }
+
+    try {
+        const signinEmail = await User.findOne({email});
+        if(!signinEmail){
+            return next(errorHandler(404, 'There is no such user!'));
+        }
+        const signinPassword = bcryptjs.compareSync(password, signinEmail.password);
+        if(!signinPassword){
+            return next(errorHandler(400, 'There is no such user!'));
+        }
+        const token = jwt.sign(
+            {id: signinEmail._id},
+            process.env.JWT_SECRET,
+        );
+        const {password: pass, ...rest} = signinEmail._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
