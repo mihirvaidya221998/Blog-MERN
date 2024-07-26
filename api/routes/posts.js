43842acfcf9fa87f122f79cb311a27a0d73150a0
Mihir Vaidya <mihirsv22@gmail.com>
2024-07-25 19:02:23 -0500

    Created api to get posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUtils.js';
-import { createPosts } from '../controllers/posts.js';
+import { createPosts, getPosts } from '../controllers/posts.js';
 
 const router = express.Router();
 
 //Create Posts
 router.post('/create-posts', verifyToken, createPosts);
+//Get all the posts
+router.get('/get-posts', getPosts);
 
 export default router; 
