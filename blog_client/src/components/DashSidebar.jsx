3deb90f5c3e97fdd4f51bff69b3d9465129a3e75
Mihir Vaidya <mihirsv22@gmail.com>
2024-08-06 12:44:08 -0500

    Added Admin Dashboard functionality

--- blog_client/src/components/DashSidebar.jsx
+++ blog_client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiChartPie } from "react-icons/hi";
 import { FaComments } from "react-icons/fa";
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -40,6 +40,13 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                {currentUser && currentUser.isAdmin && (
+                    <Link to='/dashboard?tab=dashboard'>
+                    <Sidebar.Item active={tab==='dashboard' || !tab} icon={HiChartPie} as='div'>
+                        Dashboard
+                    </Sidebar.Item>
+                </Link>
+                )}
                 <Link to='/dashboard?tab=profile'>
                     <Sidebar.Item active={tab==='profile'} icon={HiUser} label={currentUser.isAdmin?'Admin':'User'} labelColor='dark' as='div'>
                         Profile
